ed091a43ef127ead8c5c7f3187d7aa252213cb3f
Rory Poulter <rorypoulter2@gmail.com>
2024-12-13 15:13:47 +0000

    Add tests to app.test.js, add supertest to dev dependencies

---
+++ app.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const request = require('supertest');
+const app = require('./app');
+
+describe('Test the things service', () => {
+    test('GET / succeeds', () => {
+        return request(app)
+	    .get('/')
+	    .expect(200);
+    });
+
+    test('GET /stars/3 returns JSON', () => {
+        return request(app)
+	    .get('/stars/3')
+	    .expect('Content-type', /json/);
+    });
+});
