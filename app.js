218b289e100944ab49f6e48005db9bd21d2f1f3f
Rory Poulter <rorypoulter2@gmail.com>
2024-12-17 16:32:00 +0000

    Improve readability and variable names

--- app.js
+++ app.js
@@ -2,56 +2,65 @@
 const fs = require('fs');
 const express = require("express");
 const app = express();
-const jsonContent = require("./data/data.json");
+
+// Creates the file `data.json` if it does not exist
+if (!fs.existsSync("./data/data.json")){
+    let str_data = JSON.stringify({reviews:[]});
+    fs.writeFileSync("./data/data.json", str_data);
+}
+// Loads content from `data.json`
+const obj_json_content = require("./data/data.json");
 
 app.use(express.static('client'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); //Parse URL-encoded bodies
 
+// Sends the HTML body to the client when visiting the url
 app.get("/", function(req, resp){
     resp.sendFile("client/index.html");
 });
 
 // Gets the reviews with a certain amount of stars
 app.get("/stars/:stars", function(req, resp){
-    stars = parseInt(req.params.stars);
-    let data = {reviews: []};
-    for (let review of jsonContent.reviews){
-        if (review.numberStars == stars){
-            data.reviews.push(review);
+    let number_stars = parseInt(req.params.stars);
+    let obj_data = {reviews: []};
+    for (let obj_review of obj_json_content.reviews){
+        if (obj_review.numberStars == number_stars){
+            obj_data.reviews.push(obj_review);
         };
     };
-    if (data.reviews.length == 0){
-        data.reviews = null;
+    if (obj_data.reviews.length == 0){
+        obj_data.reviews = null;
     }
-    resp.send(data);
+    resp.send(obj_data);
 });
 
+// Checks the form data for repeated names and either appends to data.json or sends an error
 app.post("/review", function(req, resp){
     // Gets form data
-    let name = req.body.name;
-    let comment = req.body.comment;
-    let stars = req.body.stars;
+    let str_new_name = req.body.name.toUpperCase();
+    let str_new_comment = req.body.comment;
+    let number_new_stars = req.body.stars;
     // Formats the date into a string
-    let date = new Date().toJSON().slice(0, 10);
+    let str_current_date = new Date().toJSON().slice(0, 10);
     // Checks if there is a review with the same name
-    for (let review of jsonContent.reviews){
-        if (review.name == name){
+    for (let obj_review of obj_json_content.reviews){
+        if (obj_review.strName == str_new_name){
             resp.send("error");
             return;
         }
     }
     // Formats data as object
-    let new_review = {
-        strName: name,
-        strDate: date,
-        numberStars: stars,
-        strComment: comment
+    let obj_new_review = {
+        strName: str_new_name,
+        strDate: str_current_date,
+        numberStars: number_new_stars,
+        strComment: str_new_comment
     };
     // Pushes review to `data.json`
-    jsonContent.reviews.push(new_review);
-    let data = JSON.stringify(jsonContent);
-    fs.writeFileSync("./data/data.json", data);
+    obj_json_content.reviews.push(obj_new_review);
+    let str_data = JSON.stringify(obj_json_content);
+    fs.writeFileSync("./data/data.json", str_data);
     resp.send("success");
 })
 
